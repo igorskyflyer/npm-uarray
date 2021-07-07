7436903ac72fcd45f6e46748e39397fb2ec93e3e
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-07 15:02:38 +0200

    Update tests to support new function signature

--- test/UArray.test.js
+++ test/UArray.test.js
@@ -1,7 +1,10 @@
 const chai = require('chai').assert
 const UArray = require('../UArray')
 
+// pass an Array
 const food = UArray(['🍟', '🌭', '🍿', '🥙', '🥓'])
+// pass the entries of an Array directly = arguments
+const objects = UArray('🎈', '🎩', '⚽', '🥇', '🎯')
 
 describe('UArray tests', () => {
   it('should return an empty array', () => {
@@ -9,23 +12,57 @@ describe('UArray tests', () => {
     chai.isEmpty(UArray())
   })
 
-  it('should return "🥓"', () => {
-    chai.equal(food[-1], '🥓')
-  })
+  describe('Array[] used', () => {
+    it('should return "🥓"', () => {
+      chai.equal(food[-1], '🥓')
+    })
 
-  it('should return "🍿"', () => {
-    chai.equal(food[-3], '🍿')
-  })
+    it('should return "🍿"', () => {
+      chai.equal(food[-3], '🍿')
+    })
 
-  it('should return "🍟"', () => {
-    chai.equal(food[0], '🍟')
-  })
+    it('should return "🍟"', () => {
+      chai.equal(food[0], '🍟')
+    })
 
-  it('should return undefined', () => {
-    chai.isUndefined(food[-100])
-  })
+    it('should return undefined', () => {
+      chai.isUndefined(food[-100])
+    })
 
-  it('should return 5', () => {
-    chai.equal(food.length, 5)
-  })
+    it('should return 5', () => {
+      chai.equal(food.length, 5)
+    })
+
+    it('should return 6', () => {
+      food.push('🥟')
+      chai.equal(food.length, 6)
+    })
+  }) // array-used
+
+  describe('arguments[] used', () => {
+    it('should return "🎯"', () => {
+      chai.equal(objects[-1], '🎯')
+    })
+
+    it('should return "⚽"', () => {
+      chai.equal(objects[-3], '⚽')
+    })
+
+    it('should return "🎈"', () => {
+      chai.equal(objects[0], '🎈')
+    })
+
+    it('should return undefined', () => {
+      chai.isUndefined(objects[-100])
+    })
+
+    it('should return 5', () => {
+      chai.equal(objects.length, 5)
+    })
+
+    it('should return 6', () => {
+      objects.push('🎁')
+      chai.equal(objects.length, 6)
+    })
+  }) // arguments-passed
 })
